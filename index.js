ec252fb6063d564489d98144ec78a77dd18e3838
c-gayanclear <cgayan10@gmail.com>
2021-09-21 13:06:21 +0530

    full page ss

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const puppeteer = require('puppeteer-core');
 
 
 async function start() {
-    let launchOptions = { headless: false, 
+    let launchOptions = { 
+        headless: false, 
         executablePath: '/usr/bin/google-chrome-stable', // because we are using puppeteer-core so we must define this option
-        args: ['--start-maximized'] };
+        args: ['--start-maximized'] 
+    };
 
     const browser = await puppeteer.launch(launchOptions)
     const page = await browser.newPage()
-    await page.goto("https://dev.to/sonyarianto/practical-puppeteer-playing-with-puppeteer-core-7i0")
-    await page.screenshot({path: "dev.png"})
+    await page.goto("https://www.npmjs.com/package/puppeteer-core")
+    await page.screenshot({path: "dev.png", fullPage: true})
     await browser.close()
 }
 
